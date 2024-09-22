3cba87ca452b5b373cc732e67a4780820aa9ae53
santiago peloche <sape27@gmail.com>
2024-09-22 16:19:52 -0300

    Mejorado el componente LoadingSpinner con un mensaje de carga y optimización estética

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,21 +1,29 @@
-import { StyleSheet, ActivityIndicator, View } from 'react-native'
-import { colors } from '../global/colors'
+import React from 'react';
+import { StyleSheet, ActivityIndicator, View, Text } from 'react-native';
+import { colors } from '../global/colors';
 
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ message }) => {
     return (
         <View style={styles.container}>
-            <ActivityIndicator size={60} color="white" />
+            <ActivityIndicator size="large" color="white" />
+            
         </View>
-    )
-}
+    );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: colors.azul
-    }
-})
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.azul,
+    },
+    message: {
+        marginTop: 10,
+        fontSize: 16,
+        color: 'white',
+    },
+});
+
